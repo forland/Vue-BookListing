694378ddf896c7c6ee3a2852aa6a0ffbd28fb47d
Forland <frankforland@gmail.com>
2018-12-09 16:32:48 +0000

    with file

---
+++ src/components/BookItem.vue
@@ -0,0 +1,24 @@
+<template>
+        <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data() {
+    return {
+      name: 'BookItem',
+      props: ['book']
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+li {
+    display: block;
+    margin: 0 10px;
+}
+</style>
